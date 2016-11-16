fe062da6325e985b8eff8029995a039eb56ae42c
Boris shishov <borisshishov@gmail.com>
2016-11-16 03:37:27 +0300

    Fixed subprocess.run in python 3.4

--- portfolio/utils.py
+++ portfolio/utils.py
@@ -5,5 +5,5 @@ from django.conf import settings
 
 def url_to_pdf(url):
     output_path = os.path.join(settings.MEDIA_ROOT, 'pdf.pdf')
-    subprocess.run([settings.WKHTML2PDF_CMD, url, output_path])
+    subprocess.call([settings.WKHTML2PDF_CMD, url, output_path])
     return open(output_path, 'rb').read()
