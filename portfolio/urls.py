44f33975584e53e4c212a071a5002740719a0356
Boris shishov <borisshishov@gmail.com>
2016-11-18 22:38:34 +0300

    Removed other views for some time.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_view, name='home_view'),
-    url(r'^project/(?P<uri>[a-zA-Z0-9\_]+)', views.project_view, name='project_view'),
-    url(r'^group/(?P<uri>[a-zA-Z0-9\_]+)', views.group_view, name='group_view'),
-    url(r'^portfolio.pdf', views.pdf_view, name='portfolio-pdf'),
-    url(r'^portfolio-printable', views.pdf_view_html, name='portfolio-printable'),
+    #url(r'^$', views.home_view, name='home_view'),
+    #url(r'^project/(?P<uri>[a-zA-Z0-9\_]+)', views.project_view, name='project_view'),
+    #url(r'^group/(?P<uri>[a-zA-Z0-9\_]+)', views.group_view, name='group_view'),
+    #url(r'^portfolio.pdf', views.pdf_view, name='portfolio-pdf'),
+    #url(r'^portfolio-printable', views.pdf_view_html, name='portfolio_printable'),
+
+    url(r'^$', views.pdf_view_html, name='portfolio_printable'),
 ]
